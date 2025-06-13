c7c7b342ea877495f5f1622f1be0d391de567bc7
Als <macnigor@gmail.com>
2025-06-13 09:31:01 +0300

    add income_activity.xml add IncomeActivity.kt

--- app/src/main/java/org/macnigor/spendly/data/ui/main/MainActivity.kt
+++ app/src/main/java/org/macnigor/spendly/data/ui/main/MainActivity.kt
@@ -1,18 +1,23 @@
-package org.macnigor.spendly
+package org.macnigor.spendly.data.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.button.MaterialButton
 import kotlinx.coroutines.launch
+import org.macnigor.spendly.R
 import org.macnigor.spendly.data.database.AppDatabase
 import org.macnigor.spendly.data.database.IncomeDao
 import org.macnigor.spendly.data.database.PurchaseDao
+import org.macnigor.spendly.data.ui.income.IncomeActivity
 import org.macnigor.spendly.data.utils.Utilities
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var balanceText: TextView
+    private lateinit var incomeButton: TextView
     private lateinit var purchaseDao: PurchaseDao
     private lateinit var incomeDao: IncomeDao
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +25,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         balanceText = findViewById(R.id.balanceText)
-
         purchaseDao = AppDatabase.getDatabase(this).purchaseDao()
         incomeDao = AppDatabase.getDatabase(this).incomeDao()
+
+        findViewById<MaterialButton>(R.id.incomeButton).setOnClickListener{
+            val itent = Intent(this, IncomeActivity::class.java)
+            startActivity(itent)
+        }
+
+
+
 
         updateBalance()
     }
@@ -33,5 +45,11 @@ class MainActivity : AppCompatActivity() {
             val balance = util.updateBalance()
             balanceText.text = "%.2f ₽".format(Locale.US, balance)
         }
+    }
+
+    // функция срабатывает при переходе на этот экран
+    override fun onResume() {
+        super.onResume()
+        updateBalance()
     }
 }
